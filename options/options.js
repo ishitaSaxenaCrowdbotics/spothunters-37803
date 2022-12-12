64185425ea2b332f97fdd437fe9e1dcb53d5ebbf
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2022-12-12 12:42:28 +0100

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,13 @@
+const styles = StyleSheet.create({});
 import { StyleSheet } from "react-native";
-
 export const globalOptions = {
-  name: "parkauthority_37803",
-  url: "https://parkauthority_37803.botics.co",
-  api: "https://parkauthority_37803.botics.co/api/v1"
-}
+  "name": "parkauthority_37803",
+  "url": "https://parkauthority_37803.botics.co",
+  "api": "https://parkauthority_37803.botics.co/api/v1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
-  }
-}
+  },
+  "@modules/onboarding": {}
+};
