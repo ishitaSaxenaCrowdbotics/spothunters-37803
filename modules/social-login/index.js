cc826f4ece55a3fdae3200da5eafe70b939aef1f
Crowdbotics <team@crowdbotics.com>
2022-12-12 13:33:17 +0000

    add component Social Login

--- modules/social-login/index.js
+++ modules/social-login/index.js
@@ -1,7 +1,19 @@
-import { StyleSheet } from "react-native";
 import React from "react";
-import { View, ImageBackground, Image, Text, TouchableOpacity, ScrollView } from "react-native";
-import { NavigationHelpersContext, useNavigationBuilder, TabRouter, TabActions, createNavigatorFactory } from "@react-navigation/native";
+import {
+  View,
+  ImageBackground,
+  Image,
+  Text,
+  TouchableOpacity,
+  ScrollView
+} from "react-native";
+import {
+  NavigationHelpersContext,
+  useNavigationBuilder,
+  TabRouter,
+  TabActions,
+  createNavigatorFactory
+} from "@react-navigation/native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { createStackNavigator } from "@react-navigation/stack";
 import { BACKGROUND_URL, LOGO_URL } from "./screens/constants.js";
@@ -10,97 +22,125 @@ import { styles } from "./screens/styles";
 import { SignInTab, SignupTab } from "./screens/loginsignup";
 import PasswordReset from "./screens/reset";
 
-const LoginTabBar = ({
-  navigation,
-  state,
-  descriptors
-}) => {
+const LoginTabBar = ({ navigation, state, descriptors }) => {
   const currentTab = state.routes[state.index];
-  return <View style={styles.tabStyle}>
-      {state.routes.map(route => <View key={route.key} style={route.key === currentTab.key ? styles.activeTabStyle : null}>
-          <TouchableOpacity onPress={() => {
-        const event = navigation.emit({
-          type: "tabPress",
-          target: route.key,
-          canPreventDefault: true
-        });
-
-        if (!event.defaultPrevented) {
-          navigation.dispatch({ ...TabActions.jumpTo(route.name),
-            target: state.key
-          });
-        }
-      }}>
+  return (
+    <View style={styles.tabStyle}>
+      {state.routes.map((route) => (
+        <View
+          key={route.key}
+          style={route.key === currentTab.key ? styles.activeTabStyle : null}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              const event = navigation.emit({
+                type: "tabPress",
+                target: route.key,
+                canPreventDefault: true
+              });
+              if (!event.defaultPrevented) {
+                navigation.dispatch({
+                  ...TabActions.jumpTo(route.name),
+                  target: state.key
+                });
+              }
+            }}
+          >
             <Text style={styles.tabStyle}>
               {descriptors[route.key].options.title || route.name}
             </Text>
           </TouchableOpacity>
-        </View>)}
-    </View>;
+        </View>
+      ))}
+    </View>
+  );
 };
 
-function LoginSignupTabs({
-  initialRouteName,
-  children,
-  screenOptions
-}) {
-  const {
-    state,
-    navigation,
-    descriptors
-  } = useNavigationBuilder(TabRouter, {
+function LoginSignupTabs({ initialRouteName, children, screenOptions }) {
+  const { state, navigation, descriptors } = useNavigationBuilder(TabRouter, {
     children,
     screenOptions,
     initialRouteName
   });
-  return <NavigationHelpersContext.Provider value={navigation}>
-      <KeyboardAwareScrollView contentContainerStyle={{
-      flex: 1
-    }}>
+
+  return (
+    <NavigationHelpersContext.Provider value={navigation}>
+      <KeyboardAwareScrollView contentContainerStyle={{ flex: 1 }}>
         <ScrollView style={[styles.container]}>
-          <View style={_styles.dgijZvJL}>
+          <View style={{ flex: 1 }}>
             <View style={styles.imageContainer}>
-              <ImageBackground source={{
-              uri: BACKGROUND_URL
-            }} style={_styles.dgsYgrJb}>
-                <Image source={{
-                uri: LOGO_URL
-              }} style={_styles.hKGmYnqv} />
+              <ImageBackground
+                source={{
+                  uri: BACKGROUND_URL
+                }}
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  resizeMode: "cover",
+                  height: "100%",
+                  width: "100%"
+                }}
+              >
+                <Image
+                  source={{
+                    uri: LOGO_URL
+                  }}
+                  style={{
+                    width: 155,
+                    height: 155,
+                    alignSelf: "center",
+                    resizeMode: "contain"
+                  }}
+                />
               </ImageBackground>
             </View>
           </View>
           <View style={[styles.cardView]}>
-            <LoginTabBar navigation={navigation} state={state} descriptors={descriptors} />
+            <LoginTabBar
+              navigation={navigation}
+              state={state}
+              descriptors={descriptors}
+            />
             <View style={styles.tabContainerStyle}>
               {descriptors[state.routes[state.index].key].render()}
             </View>
           </View>
         </ScrollView>
       </KeyboardAwareScrollView>
-    </NavigationHelpersContext.Provider>;
+    </NavigationHelpersContext.Provider>
+  );
 }
 
 const createLoginNavigator = createNavigatorFactory(LoginSignupTabs);
+
 const LoginStack = createLoginNavigator();
 
 const LoginScreen = () => {
-  return <LoginStack.Navigator>
-      <LoginStack.Screen name="SignIn" component={SignInTab} options={{
-      title: "Sign In"
-    }} />
-      <LoginStack.Screen name="SignUp" component={SignupTab} options={{
-      title: "Sign Up"
-    }} />
-    </LoginStack.Navigator>;
+  return (
+    <LoginStack.Navigator>
+      <LoginStack.Screen
+        name="SignIn"
+        component={SignInTab}
+        options={{ title: "Sign In" }}
+      />
+      <LoginStack.Screen
+        name="SignUp"
+        component={SignupTab}
+        options={{ title: "Sign Up" }}
+      />
+    </LoginStack.Navigator>
+  );
 };
 
 const Stack = createStackNavigator();
 
 const LoginSignup = () => {
-  return <Stack.Navigator headerMode="none">
+  return (
+    <Stack.Navigator headerMode="none">
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
       <Stack.Screen name="PasswordReset" component={PasswordReset} />
-    </Stack.Navigator>;
+    </Stack.Navigator>
+  );
 };
 
 export default {
@@ -108,22 +148,3 @@ export default {
   navigator: LoginSignup,
   slice: slice
 };
-
-const _styles = StyleSheet.create({
-  dgijZvJL: {
-    flex: 1
-  },
-  dgsYgrJb: {
-    flex: 1,
-    justifyContent: "center",
-    resizeMode: "cover",
-    height: "100%",
-    width: "100%"
-  },
-  hKGmYnqv: {
-    width: 155,
-    height: 155,
-    alignSelf: "center",
-    resizeMode: "contain"
-  }
-});
--- modules/social-login/screens/reset.js
+++ modules/social-login/screens/reset.js
@@ -1,6 +1,12 @@
-import { StyleSheet } from "react-native";
 import React, { useState } from "react";
-import { Image, Alert, View, TouchableOpacity, TextInput, Text } from "react-native";
+import {
+  Image,
+  Alert,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Text
+} from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { useSelector, useDispatch } from "react-redux";
 import { unwrapResult } from "@reduxjs/toolkit";
@@ -8,26 +14,24 @@ import { styles, textInputStyles } from "./styles";
 import { validateEmail, LOGO_URL } from "./constants.js";
 import { resetPassword } from "../auth";
 
-const PasswordRecover = ({
-  navigation
-}) => {
+const PasswordRecover = ({ navigation }) => {
   const [email, setEmail] = useState("");
-  const {
-    api
-  } = useSelector(state => state.login);
+  const { api } = useSelector(state => state.login);
   const dispatch = useDispatch();
 
   const handlePasswordReset = () => {
-    if (!validateEmail.test(email)) {
-      return Alert.alert("Error", "Please enter a valid email address.");
-    }
+    if (!validateEmail.test(email)) { return Alert.alert("Error", "Please enter a valid email address."); }
 
-    dispatch(resetPassword({
-      email
-    })).then(unwrapResult).then(() => {
-      Alert.alert("Password Reset", "Password reset link has been sent to your email address");
-      navigation.goBack();
-    }).catch(err => console.log(err.message));
+    dispatch(resetPassword({ email }))
+      .then(unwrapResult)
+      .then(() => {
+        Alert.alert(
+          "Password Reset",
+          "Password reset link has been sent to your email address"
+        );
+        navigation.goBack();
+      })
+      .catch(err => console.log(err.message));
   };
 
   const renderImage = () => {
@@ -35,48 +39,67 @@ const PasswordRecover = ({
       width: 365,
       height: 161
     };
-    return <Image style={[styles.image, imageSize]} source={{
-      uri: LOGO_URL
-    }} />;
+    return (
+      <Image
+        style={[styles.image, imageSize]}
+        source={{
+          uri: LOGO_URL
+        }}
+      />
+    );
   };
 
-  return <View style={_styles.jSjYjzmN}>
+  return (
+    <View style={{ flex: 1 }}>
       <KeyboardAwareScrollView contentContainerStyle={styles.screen}>
         {renderImage()}
         <Text style={styles.heading}>{"Password Recovery"}</Text>
         <View style={styles.fieldContainer}>
           <Text style={styles.label}>Email Address</Text>
-          <TextInput value={email} onChangeText={value => setEmail(value)} placeholder="eg: yourname@gmail.com" size="small" style={styles.input} keyboardType="email-address" textStyle={styles.text} autoCapitalize="none" />
+          <TextInput
+            value={email}
+            onChangeText={value => setEmail(value)}
+            placeholder="eg: yourname@gmail.com"
+            size="small"
+            style={styles.input}
+            keyboardType="email-address"
+            textStyle={styles.text}
+            autoCapitalize="none"
+          />
         </View>
-        {!!api.error && <Text style={[textInputStyles.error, _styles.cEJtpJnL]}>
+        {!!api.error && (
+          <Text
+            style={[textInputStyles.error, { marginBottom: 10, fontSize: 12 }]}
+          >
             {api.error.message}
-          </Text>}
-        <TouchableOpacity disabled={api.loading === "pending"} activeOpacity={0.7} style={[styles.actionButon]} onPress={handlePasswordReset}>
-          <Text style={_styles.pLrWZEuw}>
+          </Text>
+        )}
+        <TouchableOpacity
+          disabled={api.loading === "pending"}
+          activeOpacity={0.7}
+          style={[styles.actionButon]}
+          onPress={handlePasswordReset}
+        >
+          <Text
+            style={{
+              color: "#fff",
+              fontSize: 15
+            }}
+          >
             Reset Password
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity activeOpacity={0.7} onPress={() => {
-        navigation.goBack();
-      }}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
           <Text style={[styles.textRow]}>Back to login?</Text>
         </TouchableOpacity>
       </KeyboardAwareScrollView>
-    </View>;
+    </View>
+  );
 };
 
 export default PasswordRecover;
-
-const _styles = StyleSheet.create({
-  jSjYjzmN: {
-    flex: 1
-  },
-  cEJtpJnL: {
-    marginBottom: 10,
-    fontSize: 12
-  },
-  pLrWZEuw: {
-    color: "#fff",
-    fontSize: 15
-  }
-});
